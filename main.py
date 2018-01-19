215bf6e720718afd0bdac4b05708f865904aaba5
Austin Rosenbaum <austinjrosenbaum@gmail.com>
2018-01-19 09:28:52 -0700

    Updated README and changed some spacing in main.py

--- main.py
+++ main.py
@@ -16,19 +16,6 @@ class Application(ttk.Frame):
 		self.create_widgets()
 		
 	def create_widgets(self):
-		# Add buttons
-		#self.startb = ttk.Button(self)
-		#self.startb["text"] = "Step"
-		#self.startb["command"] = self.step_prog
-		#self.startb.grid(column=0, padx=xpad, pady=ypad, row = 0)
-		#
-		#self.stopb = ttk.Button(self)
-		#self.stopb["text"] = str(self.step_val)
-		#self.stopb["command"] = self.inc_step_val
-		#self.stopb.grid(column=3, padx=xpad, pady=ypad, row = 0)
-		#
-		#self.progbar = ttk.Progressbar(self)
-		#self.progbar.grid(column=1, padx=xpad, pady=ypad, row = 0)
 		
 		# Add items containing buttons and progress bars
 		self.lemonade	= Item(self.root, "Lemonade Stand")
